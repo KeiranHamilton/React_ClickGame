2907c223143a16f6f495db24d0e3a5644c6fee91
Keiran Hamilton <keiranhamilton@gmail.com>
2019-03-15 14:17:25 -0400

    game function works, working on ui

--- react_clickgame/src/components/Header.js
+++ react_clickgame/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from "react";
+import "../styles/Header.css";
 
 // create a header with a Bootstrap navbar and jumbotron
+
 const Header = props => (
-  <div className="main-container">
+  <div className="jumbotron">
+    <h1>Click Em </h1>
+    <p className="lead">
+      Click on a picture, but dont click on the same picture twice!
+    </p>
     <nav className="navbar sticky-top navbar-light bg-light justify-content-end">
       <h5 className="text-muted">
-        Current game score: {props.currentScore} | Top score: {props.topScore}
+        Current Score: {props.currentScore} <br /> <br />
+        Top Score: {props.topScore}
       </h5>
     </nav>
-
-    <div className="jumbotron text-muted">
-      <h1>Memory Card Game</h1>
-      <p className="lead">Click a card, but only click it once!</p>
-    </div>
   </div>
 );
 
