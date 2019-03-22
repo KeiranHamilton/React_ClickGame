96bb0a623aa902b8a0f52887adf7af75490ccde6
Keiran Hamilton <keiranhamilton@gmail.com>
2019-03-21 21:22:29 -0400

    Updates

---
+++ precache-manifest.a08c920f6b60291f18063b1bfdc67f2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3db1215e3eb7a6253715",
+    "url": "/React_ClickGame/static/js/runtime~main.15b368ad.js"
+  },
+  {
+    "revision": "f1cb4f6fcac826575fba",
+    "url": "/React_ClickGame/static/js/main.7c61285c.chunk.js"
+  },
+  {
+    "revision": "17d6eb59b6c003d2b289",
+    "url": "/React_ClickGame/static/js/2.7baffc28.chunk.js"
+  },
+  {
+    "revision": "f1cb4f6fcac826575fba",
+    "url": "/React_ClickGame/static/css/main.a56fb8ec.chunk.css"
+  },
+  {
+    "revision": "6c6232140fcd9646b1b6397ecc82cfb4",
+    "url": "/React_ClickGame/index.html"
+  }
+];
